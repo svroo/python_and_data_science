455b70228146db629d4b6272248ffb27fd8f196e
Rodrigo <rod_explosionatomica@hotmail.com>
2022-07-13 18:59:04 -0500

    se agregan comentarios y se estructura mejor el codigo para hacer lo más legible

--- python_data/webscraping.py
+++ python_data/webscraping.py
@@ -6,6 +6,13 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 import pandas as pd
 
+# Definimos variables que vamos a usar despues
+partido = []
+fecha = []
+local = []
+resultado = []
+visitante = []
+
 # Definimos path de ruta del server
 driver = webdriver.Chrome("C:\\Users\\hp\\Downloads\\chromedriver")
 s = Service("C:\\Users\\hp\\Downloads\\chromedriver")
@@ -20,22 +27,20 @@ allmatches = driver.find_element(By.XPATH, '//label[@analytics-event="All matche
 allmatches.click()
 sleep(2)
 
+# Seleccionamos el boton de la region donde queremos los resultados en este caso Spain ya que el sitio esta en ingles
 dropdown = Select(driver.find_element(By.ID, "country"))
 dropdown.select_by_visible_text('Spain')
 sleep(5)
 
+# Buscamos las columnas pertenecientes a la tabla de los partidos y la almacenamos en una variable
 matches = driver.find_elements(By.XPATH, '//tr[@data-ng-repeat="match in ::dtc.$scope.matches"]')
-partido = []
 
+# Cada renglon que haya en la variabla que asignamos previamente lo vamos a convertir a texto y lo vamos a agregar a una lista 
 for match in matches:
     partido.append(match.text)
 driver.quit()
 
-fecha = []
-local = []
-resultado = []
-visitante = []
-
+# Empezamos a separar y almacenar los datos en columnas diferentes
 for i in range(0, len(partido)):
     temp = " "
     temp = str(partido[i]).strip(" ")
@@ -53,8 +58,12 @@ for i in range(0, len(partido)):
         else:
             visitante.append(temp[5])
 
+
+# Se crea un data frame con el nombre de cada columna
 df = pd.DataFrame({'fecha':fecha,
 'local':local,
 'resultado':resultado,
 'visitante':visitante})
+
+# Se guarda el dataframe
 df.to_csv('resultado.csv', index = False)
