93ff6bfaf0c875b1d574c42c4c9b0454441459a1
Rodrigo <rod_explosionatomica@hotmail.com>
2022-07-13 18:52:23 -0500

    Se modifica el de resultados de futbol ya que con la version anterior dejaba todo en una celda, en este nuevo codigo ya separa la fecha, el resultado y los equipos involucrados en cada partido, aunque siguen siendo de tipo String los valores

--- python_data/webscraping.py
+++ python_data/webscraping.py
@@ -24,13 +24,37 @@ dropdown = Select(driver.find_element(By.ID, "country"))
 dropdown.select_by_visible_text('Spain')
 sleep(5)
 
-matches = driver.find_elements(By.TAG_NAME, "tr")
-partidos = []
+matches = driver.find_elements(By.XPATH, '//tr[@data-ng-repeat="match in ::dtc.$scope.matches"]')
+partido = []
 
 for match in matches:
-    partidos.append(match.text)
+    partido.append(match.text)
 driver.quit()
 
-df = pd.DataFrame({'partidos': partidos})
-print(df)
-df.to_csv('partidos.csv', index=False)
+fecha = []
+local = []
+resultado = []
+visitante = []
+
+for i in range(0, len(partido)):
+    temp = " "
+    temp = str(partido[i]).strip(" ")
+    temp = temp.split()
+    fecha.append(temp[0])
+    if temp[1] == 'Ath' or temp[1] == 'Real':
+        local.append(temp[1] + " " + temp[2])
+        resultado.append(temp[3] + temp[4] + temp[5])
+        visitante.append(temp[6])
+    else:
+        local.append(temp[1])
+        resultado.append(temp[2] + temp[3] + temp[4])
+        if temp[5] == 'Ath'or temp[5] == 'Real':
+            visitante.append(temp[5] + " " + temp[6])
+        else:
+            visitante.append(temp[5])
+
+df = pd.DataFrame({'fecha':fecha,
+'local':local,
+'resultado':resultado,
+'visitante':visitante})
+df.to_csv('resultado.csv', index = False)
