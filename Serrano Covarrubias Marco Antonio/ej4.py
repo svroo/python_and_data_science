ed69478d8fba15fdd53f3d8784603dcd14e73073
Rodrigo <102766411+svroo@users.noreply.github.com>
2022-09-23 16:27:57 -0500

    Delete Serrano Covarrubias Marco Antonio directory

--- Serrano Covarrubias Marco Antonio/ej4.py
+++
@@ -1,6 +0,0 @@
-matriz= [[-10, 20, -30], [40,50, 60.5], [70,-80,90],[100, 110, 120]]
-
-for renglon in matriz:
-    print( )
-    for c in renglon:
-        print(c, end= ",it") 
--- Serrano Covarrubias Marco Antonio/ejpython.py
+++
@@ -1,7 +0,0 @@
-A = [[1, -2.3, 3.0, 4.5],[5.6, -6.5, 7, 8],[9.7,-10.3,11.1,12]]
-
-#Inicia ciclo para
-for i in range(3):
-    print() #salto de linea
-    for j in range (4):
-        print(A[i][j], end=",")
